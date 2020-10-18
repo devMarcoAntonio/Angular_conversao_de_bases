a6a13fb41f0473fb87adbe6e78ca4f8e972ee834
Marco Antonio <marcoantoniobraz@hotmail.com>
2020-10-18 19:18:52 -0300

    calculador de conversão de bases em ionic/angular

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -5,8 +5,45 @@ import { Component } from '@angular/core';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
+  
+  constructor(angular) {
+    let myApp = angular.module('myApp', []);
+    myApp.controller('myController'),[{
+      decimal : "",
+      binary:"",
+      octal:"",
+      hexa:"",
 
-  constructor() {}
-
-}
+     let (newValue, oldValue){
+        if (newValue != ""){
+        this.binary = parseInt(newValue, 10).toString(2);
+       this.octal = parseInt(newValue, 10).toString(8);
+        this.hexa = parseInt(newValue, 10).toString(16).toUpperCase();
+        }else{
+          this.binary="";
+          this.octal="";
+          this.hexa="";
+        }
+          
+        if (newValue != ""){
+        this.decimal = parseInt(newValue, 2).toString(10);
+      }else{
+        this.decimal = "";
+      }
+        if (newValue != ""){
+        this.decimal = parseInt(newValue, 8).toString(10);
+      }else{
+        this.decimal = "";
+      }
+        if (newValue != ""){
+        this.decimal = parseInt(newValue, 16).toString(10);
+         }else{
+        this.decimal = "";
+         }
+        }
+      }];
+    }
+  }
+  
