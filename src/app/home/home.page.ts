8bebd1c619a3622d5a963bc7e078a74b0137e315
Felipe de Almeida <felipe.almeida@thinkam.net>
2020-10-19 21:42:44 -0300

    Ajustes para execução e operação

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,43 +7,55 @@ import { Component } from '@angular/core';
 })
 
 export class HomePage {
-  
-  constructor(angular) {
-    let myApp = angular.module('myApp', []);
-    myApp.controller('myController'),[{
-      decimal : "",
-      binary:"",
-      octal:"",
-      hexa:"",
+  calculator = {
+    decimal: '',
+    binary: '',
+    octal: '',
+    hexa: '',
+    newValue: ''
+  };
 
-     let (newValue, oldValue){
-        if (newValue != ""){
-        this.binary = parseInt(newValue, 10).toString(2);
-       this.octal = parseInt(newValue, 10).toString(8);
-        this.hexa = parseInt(newValue, 10).toString(16).toUpperCase();
-        }else{
-          this.binary="";
-          this.octal="";
-          this.hexa="";
-        }
-          
-        if (newValue != ""){
-        this.decimal = parseInt(newValue, 2).toString(10);
-      }else{
-        this.decimal = "";
-      }
-        if (newValue != ""){
-        this.decimal = parseInt(newValue, 8).toString(10);
-      }else{
-        this.decimal = "";
-      }
-        if (newValue != ""){
-        this.decimal = parseInt(newValue, 16).toString(10);
-         }else{
-        this.decimal = "";
-         }
-        }
-      }];
+  constructor() {
+
+  }
+
+  reset() {
+    this.calculator = {
+      decimal: '',
+      binary: '',
+      octal: '',
+      hexa: '',
+      newValue: ''
+    };
+  }
+
+  convert() {
+    if (this.calculator.newValue !== '') {
+      this.calculator.binary = parseInt(this.calculator.newValue, 10).toString(2);
+      this.calculator.octal = parseInt(this.calculator.newValue, 10).toString(8);
+      this.calculator.hexa = parseInt(this.calculator.newValue, 10).toString(16).toUpperCase();
+    } else {
+      this.calculator.binary = '';
+      this.calculator.octal = '';
+      this.calculator.hexa = '';
+    }
+
+    if (this.calculator.newValue !== '') {
+      this.calculator.decimal = parseInt(this.calculator.newValue, 2).toString(10);
+    } else {
+      this.calculator.decimal = '';
+    }
+
+    if (this.calculator.newValue !== '') {
+      this.calculator.decimal = parseInt(this.calculator.newValue, 8).toString(10);
+    } else {
+      this.calculator.decimal = '';
+    }
+
+    if (this.calculator.newValue !== '') {
+      this.calculator.decimal = parseInt(this.calculator.newValue, 16).toString(10);
+    } else {
+      this.calculator.decimal = '';
     }
   }
-  
+}
